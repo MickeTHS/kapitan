12e11e22a9c2c2d07e12674bbb07771c862de5f4
MickeTHS <micke.nilsson.1985@gmail.com>
2020-10-27 12:30:26 +0100

    linux build

--- server/kpserver/process_stats.h
+++ server/kpserver/process_stats.h
@@ -190,93 +190,24 @@ struct Process_stats {
 
     // https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
     bool gather_stats(Process_stats_snapshot& snapshot) {
-        /// Total virtual memory
-        MEMORYSTATUSEX memInfo;
-        memInfo.dwLength = sizeof(MEMORYSTATUSEX);
-        GlobalMemoryStatusEx(&memInfo);
-        DWORDLONG totalVirtualMem = memInfo.ullTotalPageFile;
-        snapshot.ram_virt_total = totalVirtualMem;
-
-        // virtual memory currently used
-        DWORDLONG virtualMemUsed = memInfo.ullTotalPageFile - memInfo.ullAvailPageFile;
-        snapshot.ram_virt_used = virtualMemUsed;
-
-        // virtual memory used by current process
-        PROCESS_MEMORY_COUNTERS_EX pmc;
-        GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
-        SIZE_T virtualMemUsedByMe = pmc.PrivateUsage;
-        snapshot.ram_virt_process_used = virtualMemUsedByMe;
-
-        // total physical memory
-        DWORDLONG totalPhysMem = memInfo.ullTotalPhys;
-        snapshot.ram_phys_total = totalPhysMem;
-
-        // physical memory currently used
-        DWORDLONG physMemUsed = memInfo.ullTotalPhys - memInfo.ullAvailPhys;
-        snapshot.ram_phys_used = physMemUsed;
-
-        // physical memory used by current process
-        SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
-        snapshot.ram_phys_process_used = physMemUsedByMe;
-
-        // the cpu load
-        snapshot.cpu_load_total = query_global_cpu();
-
-        // cpu load for process
-        snapshot.cpu_load_process = query_process_cpu();
+        
     }
 
     void init_process_cpu_query() {
-        SYSTEM_INFO sysInfo;
-        FILETIME ftime, fsys, fuser;
-
-        GetSystemInfo(&sysInfo);
-        numProcessors = sysInfo.dwNumberOfProcessors;
-
-        GetSystemTimeAsFileTime(&ftime);
-        memcpy(&lastCPU, &ftime, sizeof(FILETIME));
-
-        self = GetCurrentProcess();
-        GetProcessTimes(self, &ftime, &ftime, &fsys, &fuser);
-        memcpy(&lastSysCPU, &fsys, sizeof(FILETIME));
-        memcpy(&lastUserCPU, &fuser, sizeof(FILETIME));
+        
     }
 
     void init_global_cpu_query() {
-        PdhOpenQuery(NULL, NULL, &cpuQuery);
-        // You can also use L"\\Processor(*)\\% Processor Time" and get individual CPU values with PdhGetFormattedCounterArray()
-        PdhAddEnglishCounter(cpuQuery, "\\Processor(_Total)\\% Processor Time", NULL, &cpuTotal);
-        PdhCollectQueryData(cpuQuery);
+        
     }
 
     double query_global_cpu() {
-        PDH_FMT_COUNTERVALUE counterVal;
-
-        PdhCollectQueryData(cpuQuery);
-        PdhGetFormattedCounterValue(cpuTotal, PDH_FMT_DOUBLE, NULL, &counterVal);
-        return counterVal.doubleValue;
+        
+        return 0;
     }
 
     double query_process_cpu() {
-        FILETIME ftime, fsys, fuser;
-        ULARGE_INTEGER now, sys, user;
-        double percent;
-
-        GetSystemTimeAsFileTime(&ftime);
-        memcpy(&now, &ftime, sizeof(FILETIME));
-
-        GetProcessTimes(self, &ftime, &ftime, &fsys, &fuser);
-        memcpy(&sys, &fsys, sizeof(FILETIME));
-        memcpy(&user, &fuser, sizeof(FILETIME));
-        percent = (sys.QuadPart - lastSysCPU.QuadPart) +
-            (user.QuadPart - lastUserCPU.QuadPart);
-        percent /= (now.QuadPart - lastCPU.QuadPart);
-        percent /= numProcessors;
-        lastCPU = now;
-        lastUserCPU = user;
-        lastSysCPU = sys;
-
-        return percent * 100;
+        return 0;
     }
 };
 #endif
